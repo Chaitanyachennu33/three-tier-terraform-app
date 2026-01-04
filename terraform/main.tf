7f624a637f9b70c228778212ad69ff1d118f2bed
Chaitanyachennu33 <116829295+Chaitanyachennu33@users.noreply.github.com>
2026-01-04 13:03:22 +0530

    Created main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecr_repository" "repo" {
+  name = "three-tier-app"
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "three-tier-cluster"
+}
